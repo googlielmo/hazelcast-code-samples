73b2d1a670e620fa3dc6ecb8e79cc81c7ef22dfe
Guglielmo Nigri <googlielmo@gmail.com>
2018-07-30 13:48:35 +0200

    TLS enabled enterprise client

--- hazelcast-integration/pcf-integration/src/main/java/com/hazelcast/pcf/integration/Application.java
+++ hazelcast-integration/pcf-integration/src/main/java/com/hazelcast/pcf/integration/Application.java
@@ -4,26 +4,59 @@ import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.client.config.ClientNetworkConfig;
 import com.hazelcast.config.GroupConfig;
+import com.hazelcast.config.SSLConfig;
 import com.hazelcast.core.HazelcastInstance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.json.BasicJsonParser;
 import org.springframework.context.annotation.Bean;
+import org.springframework.util.FileCopyUtils;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 @SpringBootApplication
 public class Application {
 
     private static ClientConfig clientConfig;
+    private static File ksFile, tsFile;
+    private static final String pwd = "123456";
 
     @Bean
     public HazelcastInstance hazelcastClient() {
+        Properties clientSslProps = new Properties();
+        clientSslProps.setProperty("javax.net.ssl.trustStore", tsFile.getAbsolutePath());
+        clientSslProps.setProperty("javax.net.ssl.trustStorePassword", pwd);
+        clientConfig.getNetworkConfig().setSSLConfig(new SSLConfig().setEnabled(true).setProperties(clientSslProps));
+        clientConfig.setLicenseKey("UNLIMITED_LICENSE#99Nodes#VuE0OIH7TbfKwAUNmSj1JlyFkr6a53911000199920009119011112151009");
         return HazelcastClient.newHazelcastClient(clientConfig);
     }
 
-    public static void main(String[] args) {
+    public static void extractKSTS() throws IOException {
+        System.out.println("extracting keystore");
+        InputStream ksStream = Application.class.getResourceAsStream("/keystore");
+        ksFile = copyToTemp(ksStream, "keystore");
+        System.out.println("extracting truststore");
+        InputStream tsStream = Application.class.getResourceAsStream("/truststore");
+        tsFile = copyToTemp(tsStream, "truststore");
+    }
+
+    private static File copyToTemp(InputStream stream, String name) throws IOException {
+        File f = File.createTempFile(name, ".dat");
+        System.out.println("writing to " + f.getAbsolutePath());
+        FileOutputStream fos = new FileOutputStream(f);
+        FileCopyUtils.copy(stream, fos);
+        fos.close();
+        return f;
+    }
+
+    public static void main(String[] args) throws IOException {
+        extractKSTS();
         String servicesJson = System.getenv("VCAP_SERVICES");
         if (servicesJson == null || servicesJson.isEmpty()) {
             System.err.println("No service found!!!");
